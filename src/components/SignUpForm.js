0d8599d3c7cb4d0d15ae4c088572e0df33b6d1d5
austenhasty <hastyae@gmail.com>
2019-07-23 10:53:20 -0700

    made profile outline and adjested some styling

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -8,20 +8,25 @@ export default class SignUpForm extends Component {
 
   render(){
     return (
-      <Form>
-        <Form.Group widths='equal'>
-          <Form.Field label='First name' control='input' placeholder='First name' />
-          <Form.Field label='Last name' control='input' placeholder='Last name' />
-          <Form.Field label='Username' control='input' placeholder='Username' />
-          <Form.Field>
-            <label>Password</label>
-            <input id="signup" name="password" type="password" placeholder="Password" />
-          </Form.Field>
-          <Form.Field>
-            <label>Confirm Password</label>
-            <input id="confirm" name="password" type="password" placeholder="Password" />
-          </Form.Field>
-        </Form.Group>
+      <Form className="signup">
+        <h2>Ready? Create Your Account Below!</h2>
+        <Form.Field>
+          <label>First Name: </label>
+          <input name="firstName" type="text" placeholder="First Name"/>
+        </Form.Field>
+        <Form.Field>
+          <label>Last Name: </label>
+          <input name="lastName" type="text" placeholder="Last Name"/>
+        </Form.Field>
+        <Form.Field>
+          <label>Username: </label>
+          <input name="username" type="text" placeholder="username"/>
+        </Form.Field>
+        <Form.Field>
+          <label>Password</label>
+          <input id="signup" name="password" type="password" placeholder="Password" />
+        </Form.Field>
+
         <Button type='submit'>Create Account</Button>
         <Divider hidden />
       </Form>
