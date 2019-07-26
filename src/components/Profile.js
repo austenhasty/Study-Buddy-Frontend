4882f3df24c959278984fd553cd388d175c65433
austenhasty <hastyae@gmail.com>
2019-07-26 14:59:42 -0700

    CRUD for topics

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,7 +7,8 @@ export default class Profile extends Component {
     super()
     this.state = {
       myTopics: [],
-      newTopic: null
+      newTopic: null,
+      editTopic: null
     }
   }
 
@@ -33,6 +34,12 @@ export default class Profile extends Component {
     })
   }
 
+  handleEditTopic= event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
   handleSubmit= (ev) => {
     ev.preventDefault()
     const token = localStorage.getItem('jwt')
@@ -49,6 +56,50 @@ export default class Profile extends Component {
     .then(res => this.setState(prevState=> ({
       myTopics: [...prevState.myTopics, res.topic]
     })))
+  }
+
+  handleDelete = (id) => {
+    const token = localStorage.getItem('jwt')
+    fetch(`http://localhost:3000/api/v1/users_topics/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': 'Bearer ' + token,
+      }
+    })
+    .then(res => res.json())
+    .then(this.setState(prevState =>({
+      myTopics: prevState.myTopics.filter(topic => topic.id !== id)
+    })))
+  }
+
+  handleEdit= (id) => {
+    const token = localStorage.getItem('jwt')
+    fetch(`http://localhost:3000/api/v1/topics/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Authorization': 'Bearer ' + token,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({name: this.state.editTopic})
+    })
+    .then(res => res.json())
+    .then(res => this.saveTopic(res))
+  }
+
+  saveTopic= res => {
+    let topicArr = this.state.myTopics.slice()
+    let level = null
+
+    for (let i=0; i < topicArr.length; i++) {
+      if (topicArr[i].id === res.topic.id) {
+        level = i
+      }
+      topicArr[level] = res.topic
+      this.setState({
+        myTopics: topicArr
+      })
+    }
   }
 
   render(){
@@ -71,7 +122,25 @@ export default class Profile extends Component {
         </Modal>
         <ul>
           {this.state.myTopics.map((topic) => {
-            return <List.Item key={topic.id} className="topicList">{topic.name}</List.Item>
+            return <List.Item key={topic.id} className="topicList">
+              {topic.name}
+              <button onClick={() => this.handleDelete(topic.id)}>Delete</button>
+              <Modal trigger={<Button>Edit Topic</Button>}>
+                <Modal.Header>Edit Your Topic:</Modal.Header>
+                <Modal.Content>
+                  <Modal.Description>
+                    <Header>Edit Your Topic:</Header>
+                    <Form>
+                      <Form.Field>
+                        <label>Topic Name:</label>
+                        <input name="editTopic" type="text" placeholder={topic.name} onChange={this.handleEditTopic}/>
+                      </Form.Field>
+                      <input type="submit" className="large ui button" value="Change Topic"  onClick={() => this.handleEdit(topic.id)}/>
+                    </Form>
+                  </Modal.Description>
+                </Modal.Content>
+              </Modal>
+            </List.Item>
           })}
         </ul>
       </div>
